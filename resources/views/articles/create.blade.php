9632e341831d53c5b9e372b058f41ee726415c01
doquyenle006 <doquyenle.006@gmail.com>
2026-06-20 22:47:39 +0700

    feet:leson3

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Tạo bài viết</title>
+</head>
+<body>
+    <h1>Tạo bài viết mới</h1>
+
+    <form>
+        <input type="text" placeholder="Tiêu đề">
+        <br><br>
+
+        <textarea placeholder="Nội dung"></textarea>
+        <br><br>
+
+        <button type="submit">Lưu</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,105 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
 
-// Trang chủ
-Route::get('/', [BlogController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Trang chủ
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
-// Trang about
+/*
+|--------------------------------------------------------------------------
+| Giới thiệu
+|--------------------------------------------------------------------------
+*/
 Route::get('/about', function () {
-    return '<h1>Về chúng tôi</h1><p>Đại học Phú Xuân</p>';
-});
+    return view('about');
+})->name('about');
 
-// Danh sách bài viết
-Route::get('/posts', [BlogController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Liên hệ
+|--------------------------------------------------------------------------
+*/
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
-// Chi tiết bài viết
-Route::get('/posts/{id}', [BlogController::class, 'show']);
+/*
+|--------------------------------------------------------------------------
+| Dữ liệu mẫu bài viết
+|--------------------------------------------------------------------------
+*/
+$articles = [
+    [
+        'id' => 1,
+        'title' => 'Giới thiệu Laravel',
+        'author' => 'Phú Xuân',
+        'date' => '20/06/2026',
+        'content' => 'Laravel là framework PHP phổ biến nhất hiện nay.'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Blade Template Engine',
+        'author' => 'Nguyễn Văn A',
+        'date' => '21/06/2026',
+        'content' => 'Blade giúp xây dựng giao diện nhanh và gọn gàng.'
+    ],
+];
+
+/*
+|--------------------------------------------------------------------------
+| Danh sách bài viết
+|--------------------------------------------------------------------------
+*/
+Route::get('/articles', function () use ($articles) {
+    return view('articles.index', compact('articles'));
+})->name('articles.index');
+
+/*
+|--------------------------------------------------------------------------
+| Chi tiết bài viết
+|--------------------------------------------------------------------------
+*/
+Route::get('/articles/{id}', function ($id) use ($articles) {
+
+    $article = collect($articles)->firstWhere('id', $id);
+
+    if (!$article) {
+        abort(404);
+    }
+
+    return view('articles.show', compact('article'));
+
+})->name('articles.show');
+
+/*
+|--------------------------------------------------------------------------
+| Form tạo bài viết
+|--------------------------------------------------------------------------
+*/
+Route::get('/articles/create', function () {
+    return view('articles.create');
+})->name('articles.create');
+
+/*
+|--------------------------------------------------------------------------
+| Shop
+|--------------------------------------------------------------------------
+*/
+Route::get('/shop/products', function () {
+
+    $products = [
+        'Laptop Dell',
+        'Bàn phím cơ',
+        'Chuột Gaming',
+        'Tai nghe Bluetooth'
+    ];
+
+    return view('shop.products', compact('products'));
+
+})->name('shop.products');
